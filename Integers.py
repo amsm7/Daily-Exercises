b02358b9a55d0647703e28bf3a170aca86b40807
amsm7 <49472469+amsm7@users.noreply.github.com>
2022-12-08 11:52:30 +0200

    Update Integers.py

--- Integers.py
+++ Integers.py
@@ -11,16 +11,28 @@ def bigger_integer(num1, num2, num3):
         max_int = num3
 
     return max_int
-
+---------------------------------------------------
 """2.
 A program that will swap between 2 numbers, 
 without a third number.
 """
 
-
 def swap_numbers(num1, num2):
     num1 = num1 + num2
     num2 = num1 - num2
     num1 = num1 - num2
-    return f"num1 new value is {num1}, num2 new value is {num2}."
+    return f"num1 new value is {num1} and num2 new value is {num2}."
+---------------------------------------------------
+"""3.
+A program that will swap between 2 numbers, 
+with a third number.
+"""
+
+def swap_numbers2(num1, num2):
+    temp = num1
+    num1 = num2
+    num2 = temp
+
+    return f"num1 new value is {num1} and num2 new value is {num2}."
+---------------------------------------------------
 
