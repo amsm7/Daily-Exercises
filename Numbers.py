863b8b7799d3ec0158b15c0d89010d2033a1887a
amsm7 <49472469+amsm7@users.noreply.github.com>
2022-12-18 12:33:40 +0200

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -35,4 +35,32 @@ def swap_numbers2(num1, num2):
 
     return f"num1 new value is {num1} and num2 new value is {num2}."
 ---------------------------------------------------
+"""4.
+Write a program that receives a number and determines
+ whether it is a prime number or not.
+ """
 
+import  math
+def check_if_prime(num):
+    if num < 2:
+        print("Smaller than 2.")
+        print("Not prime.")
+        return False
+    elif (num % 2 == 0) and (num != 2):
+        print("Even number that isn't equal to 2.")
+        print("Not prime.")
+        return False
+    elif 2 <= num <= 3:
+        print("2 or 3.")
+        print("Prime!")
+        return True
+    else:
+        num_sqrt = math.floor(math.sqrt(num))
+        for val in range(3, num_sqrt + 1, 2):
+            if num % val == 0:
+                print("Not prime.")
+                return False
+
+        print("Prime !")
+        return True
+        
