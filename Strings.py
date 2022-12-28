22b2d4de0230ec88c9619a1505b2ea3074494d9e
amsm7 <49472469+amsm7@users.noreply.github.com>
2022-12-28 09:55:52 +0200

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,1 +1,25 @@
 
+""" 1.
+A program that removes given character from a given string.
+"""
+
+def remove_character(my_str, my_char):
+    new_str = my_str.replace(my_char, '')
+    return new_str
+
+---------------------------------------------------
+""" 2.
+A program that will count the # of occurrences of a given
+char on a given string."""
+
+def count_char(my_str, my_char):
+    cnt = my_str.count(my_char)
+    return f"'{my_char}' has {cnt} occurrences in '{my_str}'."
+
+
+---------------------------------------------------
+""" 3.
+
+"""
+
+
